3afbed3fbc9eb103943e2737762c58bff74bad9e
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-20 19:37:35 -0400

    renamed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import * as React from "react";
 import Layout from "../components/layout.js";
 import "../components/layout.css";
 
-const IndexPage = () => {
+const HomePage = () => {
   return (
     <Layout pageTitle="my portfolio">
       <div className="hey-there-container">
-        <h1>yo!</h1> 
+        <h1>yo!</h1>
       </div>
     </Layout>
   )
 }
 
-export default IndexPage;
+export default HomePage;
