a43acf61add78a079c6df2a837a18d30e3cb67f8
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-21 18:19:55 -0400

    layout for home page updated to include "hey there!" section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,21 @@ const HomePage = () => {
   return (
     <Layout pageTitle="my portfolio">
       <div className="hey-there-container">
-        <h1>yo!</h1>
+        <div className="hey-there-text">
+          <h1 className="hey-there">hello there!</h1>
+          <p className="hey-there-blurb">
+            Aliquam interdum augue vel augue porta,
+            in pellentesque nunc molestie. Cras quis magna
+            ac risus auctor placerat vel vel ligula.
+            Fusce ut vestibulum diam, eget tempus risus.
+          </p>
+          <div className="home-link-container">
+            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
+            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
+            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
+          </div>
+        </div>
+        <div className="box"><div className="inner-box"></div></div>
       </div>
     </Layout>
   )
