34c164b4fdd3c9aed9970ec79e656c87dd034eeb
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-23 15:14:37 -0400

    added icons to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,43 @@ import * as React from "react";
 import Layout from "../components/layout.js";
 import "../components/layout.css";
 import CaseStudy from "../components/casestudy.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
 
 const HomePage = () => {
   return (
     <Layout pageTitle="my portfolio">
       <div className="hey-there-container">
         <div className="hey-there-text">
-          <h1 className="hey-there">hello there!</h1>
+          <h1 className="hey-there big-text">jessica zhu</h1>
           <p className="hey-there-blurb">
             Aliquam interdum augue vel augue porta,
             in pellentesque nunc molestie. Cras quis magna
             ac risus auctor placerat vel vel ligula.
             Fusce ut vestibulum diam, eget tempus risus.
           </p>
-          <div className="home-link-container">
-            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
-            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
-            <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/" target="_blank"><span className="home-link">link 1</span></a>
+          <div className="home-icons">
+            <a href="https://github.com/jezhu8928/" target="_blank" className="home-icon-link">
+              <FontAwesomeIcon icon={faGithub} className="regular-icon"/>
+            </a>
+            <a href="https://www.linkedin.com/in/jessicajlzhu/" target="_blank" className="home-icon-link">
+              <FontAwesomeIcon icon={faLinkedin} className="regular-icon"/>
+            </a>
+            <a href="mailto:jessica.zhu@uwaterloo.ca" target="_blank" className="home-icon-link">
+              <FontAwesomeIcon icon={faEnvelope} className="regular-icon"/>
+            </a>
           </div>
         </div>
-        <div className="box"><div className="inner-box"></div></div>
+        <div className="box"></div>
       </div>
       <div className="portfolio-container">
         <div className="work-cases">
           <h2 className="subheadings">work</h2>
-
           <CaseStudy pageLink="https://www.theringer.com/" title="making reading fun again" company="just for lolz" tags="react, js"/>
           <CaseStudy pageLink="https://www.theringer.com/" title="making reading fun again" company="just for lolz" tags="react, js"/>
           <CaseStudy pageLink="https://www.theringer.com/" title="making reading fun again" company="just for lolz" tags="react, js"/>
-
-
         </div>
         <div className="side-projects">
           <h2 className="subheadings">side projects</h2>
