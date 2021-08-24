023169a8d97b7a23629a5af7f053dad046873a0e
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-24 15:26:53 -0400

    custom 404 page added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,14 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import Layout from "../components/layout.js";
+import {Link} from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function Component () {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <Layout pageTitle="page not found!">
+      <div className="help-container">
+        <h2 className="help-message">404 &mdash; there's no page at that link! 🥸</h2>
+        <p className="help-instruction">try using the navigation bar above to navigate, or <Link to="/" className="help-link">return home</Link></p>
+      </div>
+    </Layout>
+  );
 }
-
-export default NotFoundPage
