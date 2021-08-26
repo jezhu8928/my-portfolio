a933e6d88430226cfe1b203798151059b58974d8
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-26 13:29:43 -0400

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,17 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/images/matcha-icon.png",
+      },
+    },
   ],
 };
