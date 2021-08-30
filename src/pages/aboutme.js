580dd0475c6415432495dd3dc666010a2110d6cb
Jessica Zhu <jessica.jz.zhu@gmail.com>
2021-08-30 18:11:17 -0400

    updated about me page

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -11,27 +11,53 @@ const AboutPage = () => {
         <div className="bio-text">
           <h1> i'm jessica &mdash;</h1>
           <p className="bio-blurb">
-            Vestibulum ante ipsum primis in faucibus orci
-            luctus et ultrices posuere cubilia curae;
-            Etiam felis metus, feugiat et volutpat vel,
-            aliquet eget mauris. Sed id orci lobortis erat
-            ornare tincidunt. Praesent quis tincidunt sapien.
-            Curabitur dolor lorem, posuere at interdum vel,
-            vehicula id.
+            matcha lover, amateur chef, nba nerd.
             <br/><br/>
-            Vestibulum ante ipsum primis in faucibus orci luctus
-            et ultrices posuere cubilia curae; Etiam felis metus,
-            feugiat et volutpat vel, aliquet eget mauris.
+            but more importantly, i'm a first year engineering student
+            who was drawn to design and development in an effort to redefine
+            how we connect with each other on the interwebs.
+
             <br/><br/>
-            Vestibulum
-            ante ipsum primis in faucibus orci luctus
-            et ultrices posuere cubilia curae; Etiam felis metus.
+            in my free time, you'll find me <a
+              href="https://sphenoid-waterfall-b15.notion.site/hey-it-s-crochet-060421cf12484c72b1c335657563f990"
+              target="_blank"
+              rel="noreferrer"
+              className="about-me-link"
+            >crocheting</a>, <a
+              href="https://letterboxd.com/jesszhu/"
+              target="_blank"
+              rel="noreferrer"
+              className="about-me-link">watching movies</a> or shooting hoops at the park.
+            <br/><br/>
+            recent recommendations:
+            <br/>
+            <a
+              href="https://letterboxd.com/film/emma-2020/"
+              target="_blank"
+              rel="noreferrer"
+              className="recommendation"
+            >&emsp;👀&ensp;emma.</a>
+            <br/>
+            <a
+              href="https://open.spotify.com/album/0QnWm07An8WyO3eupjBDoP"
+              target="_blank"
+              rel="noreferrer"
+              className="recommendation"
+            >&emsp;🎧&ensp;still woozy</a>
+            <br/>
+            <a
+              href="https://www.goodreads.com/book/show/52578297-the-midnight-library"
+              target="_blank"
+              rel="noreferrer"
+              className="recommendation"
+            >&emsp;📚&ensp;the midnight library</a>
+            <br/>
           </p>
         </div>
         <div className="bio-image-container">
           <figure>
             <img src={bballPic} alt="me playing basketball" className="bio-image"/>
-            <figcaption>placeholder caption</figcaption>
+            <figcaption>me playing basketball in high school</figcaption>
           </figure>
         </div>
       </div>
@@ -39,26 +65,39 @@ const AboutPage = () => {
       <div className="resumeish">
         <div className="experience-section">
           <h2 className="subheadings">experience</h2>
+          <Experience location="spartan sports association" position="member" date="june 2018 - june 2021">
+            <li>collaborated with a <b>40 member council of leading student athletes</b> to run school athletic events, promote spirit and athletics.</li>
+          </Experience>
           <Experience location="shad" position="participant" date="summer 2020">
-            <li>led design team in creation of</li>
+            <li>remotely led 8-member team to design a <b>simulated circulatory system</b> intended for spaceflight</li>
           </Experience>
-          <Experience location="spartahacks" position="event organizer" date="november 2018-december 2019">
-            <li>hosted hackathon for ~100 participants</li>
+          <Experience location="spartahacks" position="event organizer" date="november 2018 - december 2019">
+            <li>worked with small peer team to host hackathon <b>(~100 attendees)</b></li>
           </Experience>
-          <Experience location="varsity girls volleyball team" position="mvp right side, left side, setter" date="dec 2019-feb 2020">
-            <li>played variety of positions</li>
+          <Experience location="grad committee" position="admin. coordinator" date="october 2020 - june 2021">
+            <li>coordinated large-scale virtual events to keep student body engaged during pandemic</li>
+          </Experience>
+          <Experience location="chemistry club" position="executive" date="september 2019 - june 2021">
+            <li>organized experiment demonstrations, planned and participated in science olympics events</li>
+          </Experience>
+          <Experience location="ccvi junior/varsity girls volleyball team" position="right side/setter" date="december 2019 - february 2020">
+            <li>named <b>MVP</b>, All Star in consecutive indoor seasons for <b>consistent play, versatility</b></li>
+          </Experience>
+          <Experience location="ccvi varsity girls basketball team" position="guard" date="september 2018 - november 2019">
+            <li>named <b>All Star</b>, helping lead team to 2nd place in district in consecutive seasons</li>
           </Experience>
 
         </div>
         <div className="education-section">
           <h2 className="subheadings">education</h2>
-          <Experience location="university of waterloo" position="BASc. candidate" date="november 2018-december 2019">
-            <li>hosted hackathon for ~100 participants</li>
+          <Experience location="university of waterloo" position="BASc. candidate" date="class of 2026">
+            <li>received <b>President’s Scholarship of Distinction</b></li>
+            <li>courses: digital computation, introduction to design</li>
           </Experience>
-          <Experience location="centennial cvi" position="high school" date="november 2018-december 2019">
-            <li>graduated with a grade 12 average of 99.8%</li>
-            <li>earned <b>top scholar</b> from grades 9-12 (top average award)</li>
-            <li>relevant extra-curriculars: cs club, chemistry club</li>
+          <Experience location="centennial cvi" position="ontario secondary school diploma" date="class of 2021">
+            <li>earned <b>top scholar</b> from grades 9-12 (grade 12 average: <b>99.7%</b>)</li>
+            <li>achieved <b>proficiency</b> in English, French, math, science and business</li>
+            <li>won <b>NCWIT Award for Aspirations in Computing</b> for pursuit of STEM as a woman</li>
           </Experience>
         </div>
       </div>
